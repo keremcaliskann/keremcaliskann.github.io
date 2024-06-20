fb67b6316ba8e413a473f6e2a51abd2e55b0529e
Kerem Çalışkan <caliskan.kerem.57@gmail.com>
2024-06-20 12:18:40 +0300

    added js

---
+++ games.js
@@ -0,0 +1,106 @@
+const data = [
+    { 
+        title: "Bullet Evolve",
+        link: "https://play.google.com/store/apps/details?id=com.bp.bulletevolve",
+    },
+    { 
+        title: "Join The Jelly",
+        link: "https://play.google.com/store/apps/details?id=com.ToonGames.JoinTheJelly",
+    },
+    { 
+        title: "WC Line",
+        link: "https://play.google.com/store/apps/details?id=com.ToonGames.wcline",
+    },
+    { 
+        title: "Pack The Bus",
+        link: "https://play.google.com/store/apps/details?id=com.ToonGames.PackTheBus",
+    },
+    { 
+        title: "Knit Tangled",
+        link: "https://play.google.com/store/apps/details?id=com.ToonGames.KnitTangled",
+    },
+    { 
+        title: "Tower Rush",
+        link: "https://play.google.com/store/apps/details?id=com.arkhe.towerrush",
+    },
+    { 
+        title: "Hammer Run",
+        link: "https://play.google.com/store/apps/details?id=com.ToonGames.HammerRun",
+    },
+    { 
+        title: "Fire Run",
+        link: "https://play.google.com/store/apps/details?id=com.ToonGames.FireRun",
+    },
+    { 
+        title: "Water Balloon Run",
+        link: "https://play.google.com/store/apps/details?id=com.ToonGames.WaterBalloonRun",
+    },
+    { 
+        title: "Merge Car Convoy!", 
+        link: "https://play.google.com/store/apps/details?id=com.honeycomb.mergeconvoy",
+    },
+    { 
+        title: "Zombie Roller", 
+        link: "https://apkcombo.com/tr/zombie-roller-crush-em-all/com.honeycomb.zombieroller/",
+    },
+    { 
+        title: "Zombie Raid", 
+        link: "https://play.google.com/store/apps/details?id=com.honeycomb.zombieraid",
+    },
+    { 
+        title: "Dash Agent", 
+        link: "https://apkcombo.com/tr/dash-agent/com.sharkfin.dashagent/",
+    },
+    { 
+        title: "Rope Crowd", 
+        link: "https://apkcombo.com/tr/rope-crowd-raid/com.sharkfin.ropecrowd/",
+    },
+    { 
+        title: "Bodyguard", 
+        link: "https://apkcombo.com/tr/bodyguard-io/com.fbg.bodyguard/",
+    },
+    { 
+        title: "Food Survivor", 
+        link: "https://apkcombo.com/tr/food-survivor-io/com.fbg.foodsurvivor/",
+    },
+    { 
+        title: "Restoration Master", 
+        link: "https://apkcombo.com/tr/restoration-master/com.fbg.restoration/",
+    },
+    { 
+        title: "Merge Animals Village", 
+        link: "https://apkcombo.com/tr/merge-animals-village/com.fbg.mergeanimalvillage/",
+    },
+    { 
+        title: "Blob Animal Fight", 
+        link: "https://apkcombo.com/tr/blob-animal-fight/com.fbg.blobmonster/",
+    },
+    { 
+        title: "Merge Animal Fight", 
+        link: "https://apkcombo.com/tr/merge-animal-fight-3d/com.fbg.mergeanimalfight/",
+    },
+    { 
+        title: "Bullet Shift", 
+        link: "https://apkcombo.com/tr/bullet-shift/com.fbg.bulletshift/",
+    },
+    { 
+        title: "Wedding Decorate", 
+        link: "https://apkcombo.com/tr/wedding-decorate/com.sharkfin.wedding",
+    },
+    { 
+        title: "Animal Limbs", 
+        link: "https://apkcombo.com/tr/animal-limbs-merge-fight/com.honeycomb.animallimbs/",
+    },
+    { 
+        title: "Train Shooter", 
+        link: "https://apkcombo.com/tr/train-shooter/com.fbg.trainshooter/",
+    },
+    { 
+        title: "Emoji Throw", 
+        link: "https://www.youtube.com/watch?v=jVn_7W-T0yw&ab_channel=SunnyMobile",
+    },
+    { 
+        title: "Fat Guy Rush", 
+        link: "https://apkcombo.com/tr/fat-guy-rush/com.fbg.fatguyrush/",
+    }
+];
---
+++ index.js
@@ -0,0 +1,47 @@
+function populateContent(item) {
+
+    const li = document.createElement('li');
+    li.className = 'project-item  active';
+
+    const a = document.createElement('a');
+    a.href=item.link;
+    a.target = '_blank';
+    
+    const figure = document.createElement('figure');
+    figure.className='project-img';
+    
+    const div = document.createElement('div');
+    div.className='project-item-icon-box';
+    
+    const ionIcon = document.createElement('ion-icon');
+    ionIcon.name='eye-outline';
+    
+    const img = document.createElement('img');
+    img.src= 'icons/'+item.title+'.png';
+    img.loading='lazy';
+    
+    const h3 = document.createElement('h3');
+    h3.className='project-title';
+    h3.textContent=item.title;
+
+    li.appendChild(a);
+
+    a.appendChild(figure);
+    a.appendChild(h3);
+
+    figure.appendChild(div);
+    figure.appendChild(img);
+
+    div.appendChild(ionIcon);
+
+    const ul = document.getElementById('item');
+    ul.appendChild(li);
+}
+
+function load(){
+    data.forEach(item=>{
+        populateContent(item);
+    });
+}
+
+window.onload = load;
